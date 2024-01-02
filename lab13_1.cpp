2463e2a655b9a8ce65294cb7e173084c918bb6c1
HaHaHaHugo <Navapol_naksen@cmu.ac.th>
2024-01-02 17:09:11 +0700

    lab14_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,30 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double A[],int N,double B[]){
+    double sum=0, sumsd=0, sumgm = 1, sumhm=0;
+    B[4]=A[0];
+    B[5]=A[0];
+    for(int i=0; i<N; i++){
+        sum += A[i];
+        B[0] = sum/N;
+    }
+    for(int i=0; i<N; i++){
+        sumsd += pow(A[i]-B[0],2);
+        B[1] = pow(sumsd/N,0.5);
+    }
+    for(int i=0; i<N; i++){
+        sumgm *= A[i];
+        B[2] = pow(sumgm,1.0/N);
+    }
+    for(int i=0; i<N; i++){
+        sumhm += 1.0/A[i];
+        B[3] = N/sumhm;
+    }
+    for(int i=0; i<N; i++){
+        if(B[4]<A[i]) B[4]=A[i];
+    }
+    for(int i=0; i<N; i++){
+        if(B[5]>A[i]) B[5]=A[i];
+    }
+}
